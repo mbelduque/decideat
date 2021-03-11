5bac41354c3bd260d9c73112801b9cc3551760a9
mbelduque <m.belduque@hotmail.com>
2021-03-11 00:40:19 -0500

    Login advance

--- screens/account/Login.js
+++ screens/account/Login.js
@@ -1,12 +1,57 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Divider } from "react-native-elements";
 
 export default function Login() {
-    return (
-        <View>
-            <Text>Login</Text>
-        </View>
-    )
+  return (
+    <ScrollView>
+      <Image
+        source={require("../../assets/large_datahungry.png")}
+        resizeMode="contain"
+        style={styles.image}
+      />
+      <View style={styles.container}>
+        <Text>Login Form</Text>
+        <CreateAccount />
+      </View>
+      <Divider style={styles.divider} />
+    </ScrollView>
+  );
 }
 
-const styles = StyleSheet.create({})
+function CreateAccount(props) {
+  return (
+    <Text style={styles.register} onPress={() => console.log("registrar")}>
+      ¿Aún no tienes una cuenta?{" "}
+      <Text style={styles.btnRegister}>Regístrate</Text>
+    </Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    height: 150,
+    width: "100%",
+    marginBottom: 20,
+  },
+
+  container: {
+    marginHorizontal: 40,
+  },
+
+  divider: {
+    backgroundColor: "#f36b24",
+    margin: 40,
+  },
+
+  register: {
+    marginTop: 15,
+    marginHorizontal: 10,
+    alignSelf: "center",
+  },
+
+  btnRegister: {
+    color: "#f36b24",
+    fontWeight: "bold",
+  },
+});
