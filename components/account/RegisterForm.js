a5bf8cff06e09569cc2e9e6727054107cb37749b
mbelduque <m.belduque@hotmail.com>
2021-03-18 00:17:43 -0500

    save credentials on state

--- components/account/RegisterForm.js
+++ components/account/RegisterForm.js
@@ -4,14 +4,30 @@ import { Button, Icon, Input } from "react-native-elements";
 
 export default function RegisterForm() {
   const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState("");
+
+  const defaultFormValues = () => {
+    return { email: "", password: "", confirm: "" };
+  };
+
+  const onChange = (e, type) => {
+    setFormData({ ...formData, [type]: e.nativeEvent.text });
+  };
+
   return (
     <View style={styles.form}>
-      <Input containerStyle={styles.input} placeholder="Ingresa tu email" />
+      <Input
+        containerStyle={styles.input}
+        placeholder="Ingresa tu email"
+        onChange={(e) => onChange(e, "email")}
+        keyboardType="email-address"
+      />
       <Input
         placeholder="Ingresa tu contraseña"
         containerStyle={styles.input}
         password={true}
         secureTextEntry={!showPassword}
+        onChange={(e) => onChange(e, "password")}
         rightIcon={
           <Icon
             type="material-community"
@@ -26,6 +42,7 @@ export default function RegisterForm() {
         containerStyle={styles.input}
         password={true}
         secureTextEntry={!showPassword}
+        onChange={(e) => onChange(e, "confirm")}
         rightIcon={
           <Icon
             type="material-community"
@@ -39,6 +56,7 @@ export default function RegisterForm() {
         title="Registrar nuevo usuario"
         containerStyle={styles.btnContainer}
         buttonStyle={styles.button}
+        onPress={() => console.log(formData)}
       />
     </View>
   );
